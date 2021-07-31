e2fba47a661dd5016e38a588e8a7ee2ac2683388
sowmya reddy <atlasowmya14@gmail.com>
2021-07-30 19:32:07 -0500

    User can compose a tweet

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -19,10 +19,15 @@ class HomeTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loadTweets()
+        
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
 
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadTweets()
     }
 
     
